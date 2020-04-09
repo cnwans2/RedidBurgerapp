10e4bf035ff444e3ea528e0f266412eb7ec66c16
Cnwans2 <crystal.nwansi26@gmail.com>
2020-04-08 20:20:45 -0500

    changed stuff

--- db/schema.sql
+++ db/schema.sql
@@ -2,7 +2,7 @@ CREATE DATABASE burgers_db;
 use burgers_db;
 
 CREATE TABLE burger2 (
- id int NOT NULL AUTO_INCREMENT,
+ id int NOT NULL 
     burger_name varchar(30) NOT NULL,
     devoured BOOLEAN,
     PRIMARY KEY (id)
--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 var express = require("express");
-
-
+ar exphbs = require("express-handlebars")
 var PORT = process.env.PORT || 8000;
 var app = express();
+
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
