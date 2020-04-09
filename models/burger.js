2fb98ba01eb4cbf92c8fd03dff674e6fc2e52d04
Cnwans2 <crystal.nwansi26@gmail.com>
2020-04-08 20:39:03 -0500

    sum

---
+++ models/burger.js
@@ -0,0 +1,9 @@
+var orm = require("../config/orm.js");
+
+var burger = {
+    selectAll: function(cb) {
+      orm.selectAll("burgers", function(res) {
+        cb(res);
+      });
+    },
+  
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 var express = require("express");
-ar exphbs = require("express-handlebars")
+ar exphbs = require("express-handlebars");
 var PORT = process.env.PORT || 8000;
 var app = express();
 
